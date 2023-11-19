9827aa4b26e10c696d0c704fb0a82a6f247b3a56
Caio Souza <113695603+Caiofz@users.noreply.github.com>
2023-11-19 02:05:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function listarFilmes(){
+    var lista;
+    lista = "Os melhores filmes você encontra aqui!\n\nBirdBox\nO Poço\nA Barraca do Beijo";
+    alert(lista);
+}
+
+function listarSeries(){
+    var lista;
+    lista = "As melhores séries você encontra aqui!\n\n13 Reasons Why\nHeart Break High\nElite";
+    alert(lista);
+}
+
+function listarDesenhos(){
+    var lista;
+    lista = "Os melhores desenhos você encontra aqui!\n\nNimona\nNossa Casa\nTuca & Bertie";
+    alert(lista);
+}
